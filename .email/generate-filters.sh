16c62ab46b9a61390fcdc16b897204a06190f903
revett <2796074+revett@users.noreply.github.com>
2021-10-26 22:44:46 +0100

    Update email lists

---
+++ .email/generate-filters.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# GMail Filter Generation
+#
+# This script generates a number of GMail compatible filters, aimed at helping
+# to manage unwanted emails. Currently it supports:
+#
+# - blocklist: List of domains which are known for recruiter spam etc
+# - allowlist: List of domains which are known to send valuable emails
+#
+# Usage: ./generate-filters.sh
+#
+# @author Charlie Revett
+
+# TODO: Move this to a separate Github repo.
+f="$HOME/dotfiles/.email/blocklist.txt"
+r=""
+i=0
+
+echo "> generating $f"
+echo "> sorting (a-z) lines"
+sort -o $f $f
+
+while IFS= read -r domain; do
+  ((i=i+1))
+
+  if [ $i -eq 1 ]; then
+    r="$domain"
+    continue
+  fi
+
+  r="$r OR $domain"
+done < "$f"
+
+echo -e "> filter:\n"
+printf -- "$r\n\n"
+
+f="$HOME/dotfiles/.email/allowlist.txt"
+r=""
+i=0
+
+echo "> generating $f"
+echo "> sorting (a-z) lines"
+sort -o $f $f
+
+while IFS= read -r domain; do
+  ((i=i+1))
+
+  if [ $i -eq 1 ]; then
+    r="-$domain"
+    continue
+  fi
+
+  r="$r -$domain"
+done < "$f"
+
+echo -e "> filter:\n"
+printf -- "$r"
