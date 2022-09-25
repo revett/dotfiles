12e41786b6a5e0c82318d6c4578d52006e75a128
revett <2796074+revett@users.noreply.github.com>
2022-09-25 23:35:38 +0100

    Update paths within install.sh to not use ~/.dotfiles

--- install.sh
+++ install.sh
@@ -8,21 +8,24 @@ echo "> starting install.sh"
 echo "> creating symlinks"
 
 # Bash
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/projects/github.com/revett/.bash_profile ~/.bash_profile
+ln -sf ~/projects/github.com/revett/dotfiles/.zshrc ~/.zshrc
 
 # Git
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/projects/github.com/revett/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/projects/github.com/revett/dotfiles/.gitignore ~/.gitignore
 
 # Starship
-ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
+ln -sf ~/projects/github.com/revett/dotfiles/starship.toml ~/.config/starship.toml
 
 # Vim
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/projects/github.com/revett/dotfiles/.vimrc ~/.vimrc
 
 # VSCode
-ln -sf ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/
-ln -sf ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
+ln -sf ~/projects/github.com/revett/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User/
+ln -sf ~/projects/github.com/revett/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User/
+
+# Hops
+ln -sf ~/projects/github.com/revett/dotfiles/.hops.yml ~/.hops.yml
 
 echo "> complete"
