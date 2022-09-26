2f025cfc7cdc4d6f1cca0710f77402228f429dbb
revett <2796074+revett@users.noreply.github.com>
2022-09-26 18:23:03 +0100

    Add support to Config for taps, as well as installing casks.

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,10 +10,11 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// Config holds the list of Homebrew packages (formulae and casks) to install
-// locally.
+// Config holds the list of Homebrew packages (formulae and casks) and taps to
+// install locally.
 type Config struct {
 	Formulae []string `yaml:"formulae"`
+	Taps     []string `yaml:"taps"`
 	Casks    []string `yaml:"casks"`
 }
 
@@ -44,20 +45,31 @@ func Load() (Config, error) {
 		return Config{}, fmt.Errorf("length of formulae list in config is zero")
 	}
 
+	if len(config.Taps) == 0 {
+		return Config{}, fmt.Errorf("length of taps list in config is zero")
+	}
+
 	if len(config.Casks) == 0 {
 		return Config{}, fmt.Errorf("length of casks list in config is zero")
 	}
 
 	config.Formulae = slice.Dedup(config.Formulae)
+	config.Taps = slice.Dedup(config.Taps)
 	config.Casks = slice.Dedup(config.Casks)
 
 	return config, nil
 }
 
+// Log outputs the contents of the config to the log.
 func (c Config) Log() {
 	log.Info().Msgf("%d formulae found in config", len(c.Formulae))
 	for _, f := range c.Formulae {
 		fmt.Printf(" %s\n", f) //nolint:forbidigo
+	}
+
+	log.Info().Msgf("%d taps found in config", len(c.Taps))
+	for _, t := range c.Taps {
+		fmt.Printf(" %s\n", t) //nolint:forbidigo
 	}
 
 	log.Info().Msgf("%d casks found in config", len(c.Casks))
