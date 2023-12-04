72e3aedc7aea2b71ad253b6d76a1d5a46a90257a
revett <2796074+revett@users.noreply.github.com>
2023-12-04 11:12:24 +0000

    Raycast: Extension to Approve PRs faster (prcake)

---
+++ raycast/run-prcake.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Approve PR with :cake: (prcake)
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🍰
+# @raycast.argument1 { "type": "text", "placeholder": "PR Number", "required": true }
+# @raycast.argument2 { "type": "text", "placeholder": "Message (optional)", "optional": true }
+# @raycast.argument3 { "type": "text", "placeholder": "Repo (default: core)", "optional": true }
+
+# Documentation:
+# @raycast.description Use this command to approve a PR with a :cake: emoji.
+# @raycast.author Charlie Revett (@revcd)
+# @raycast.authorURL https://revcd.com
+
+source /Users/revett/projects/github.com/revett/dotfiles/.functions
+prcake "$1" "$3" "$2"
+echo "🍰 Approved PR #$1"
